5bf4ff021a47576acab32bf75b8f12243e531360
saeedghadiri <saeed.ghadiri@gmail.com>
2025-05-18 11:32:50 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pytimeir",
-    version="0.2",
+    version="0.3",
     author="Saeed Ghadiri",
     author_email="saeed.ghadiri@gmail.com",
     description="Get events and holidays from time.ir",
